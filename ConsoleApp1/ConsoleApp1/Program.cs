d23e33a04d77df940fbbf2432c4f4fa594b3dd63
עידן שמי <doshi050@walla.co.il>
2020-09-03 00:50:25 +0300

    commit1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I will be using Github this year.");
+        }
+    }
+}
